7bca4e36e268300141cf8bd6e46d4eef2107c496
nhanpnt91 <43132745+nhanpnt91@users.noreply.github.com>
2023-09-23 05:15:48 +0000

    added some content

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs. 10,000,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India',
+    'salary':  'Rs. 15,000,000'
+  }
+]
+
 @app.route('/')
-def hello_world():
-  return "Hello World"
+def hello_me():
+  return render_template('home.html', jobs=JOBS)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
